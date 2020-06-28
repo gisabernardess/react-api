7d1f979fa946c3975e4bf187516895fc00c088f9
Gisele Bernardes da Silva <giiselebernardes@gmail.com>
2020-06-28 20:55:42 -0300

    rest api call

--- src/view/dashboard/DashboardView.tsx
+++ src/view/dashboard/DashboardView.tsx
@@ -1,31 +1,61 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { Title, Form, Repositories } from './styles';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import api from '../../services/api';
+
+interface Repository {
+  id: string;
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+  html_url: string;
+}
 
 export function DashboardView() {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  const handleAddRepository = async (
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> => {
+    event.preventDefault();
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+    setRepositories([...repositories, response?.data]);
+  };
+
   return (
     <>
       <Title>Explore repositórios no Github</Title>
-      <Form action="">
-        <input type="text" placeholder="Digite o nome do repositório" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          type="text"
+          placeholder="octocat/Hello-World"
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+        />
         <button type="submit">
           <FontAwesomeIcon icon={faSearch} size="lg" />
         </button>
       </Form>
 
       <Repositories>
-        <a href="/">
-          <img
-            src="https://api.adorable.io/avatars/64/abott@adorable.png"
-            alt="user-name"
-          />
-          <div>
-            <strong>title</strong>
-            <p>description</p>
-          </div>
-          <FontAwesomeIcon icon={faChevronRight} />
-        </a>
+        {repositories.map((repository) => (
+          <a href={repository.html_url} key={repository.id} target="blank">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FontAwesomeIcon icon={faChevronRight} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
